8681ed856cb1170d056d060a4cd37468094f2b71
Daniela <daniela.paz.jerez@gmail.com>
2020-08-01 16:53:15 -0400

    fotos estructura index inicio js y css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-hola
+
+
+//FUNCION POPOVERS BOOTSTRAP
+$(function () {
+	$('[data-toggle="popover"]').popover()
+})
+
+//VENTANAS MODALES
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
+
+//TOOLTIP
+$(function () {
+$('[data-toggle="tooltip"]').tooltip()
+})
